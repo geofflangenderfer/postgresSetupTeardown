3e9bc341a244912b2190f36f7cbd452967943553
Geoff Langenderfer <geoff.langenderfer@gmail.com>
2020-04-14 12:17:41 -0500

    cleaned up

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-./testQueries.sh > successfulOutput.txt 2> errors.txt
+./testQueries.sh &> output.txt 
 
--- testQueries.sh
+++ testQueries.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+postgresDbLogin='postgresql://postgres:postgres@localhost:5432/postgres'
+testDbLogin='postgresql://postgres:postgres@localhost:5432/test'
+pathToData="/home/geoff/work/UT/homeworkGrades/09-sql/bashScripts/data" # update
+
 main() {
   echo "db refresh..."
   echo "---------------------------------------------"
@@ -7,42 +11,40 @@ main() {
   echo "---------------------------------------------"
   echo "creating tables..."
   echo "---------------------------------------------"
-  createTables TableCreation.sql # must update for each student
+  loadFile DB_Creation.sql # must update for each student
   echo "---------------------------------------------"
-  #queryTableNames 
+  queryTableNames 
   echo "loading data..."
   echo "---------------------------------------------"
-  loadData
+  loadData # must update for different table names
   echo "---------------------------------------------"
   echo "querying tables..."
   echo "---------------------------------------------"
-  queryTablesWithFile combinedAnalysis.sql # must update for each student
+  loadFile DB_Query.sql # must update for each student
   echo "---------------------------------------------"
   echo "db refresh..."
   echo "---------------------------------------------"
-  refreshDb
+  #refreshDb
 }
 refreshDb() {
-  psql -c "drop database test;" postgresql://postgres:postgres@localhost:5432/postgres
-  psql -c "create database test;" postgresql://postgres:postgres@localhost:5432/postgres
+  psql -c "drop database test;" $postgresDbLogin
+  psql -c "create database test;" $postgresDbLogin
 }
-createTables() {
-  psql -f $1 postgresql://postgres:postgres@localhost:5432/test
+getColumnInfo() {
+  psql -c "\d+ $1" $postgresDbLogin
+}
+loadFile() {
+  psql -f $1 $testDbLogin
 }
 loadData() {
-  pathToData="/home/geoff/work/UT/homeworkGrades/09-sql/bashScripts/data" 
-
-  psql -c "\copy departments from '$pathToData/departments.csv' with (format csv, header true);" postgresql://postgres:postgres@localhost:5432/test
-  psql -c "\copy employees from '$pathToData/employees.csv' with (format csv, header true);" postgresql://postgres:postgres@localhost:5432/test
-  psql -c "\copy dept_emp from '$pathToData/dept_emp.csv' with (format csv, header true);" postgresql://postgres:postgres@localhost:5432/test
-  psql -c "\copy dept_manager from '$pathToData/dept_manager.csv' with (format csv, header true);" postgresql://postgres:postgres@localhost:5432/test
-  psql -c "\copy salaries from '$pathToData/salaries.csv' with (format csv, header true);" postgresql://postgres:postgres@localhost:5432/test
-  psql -c "\copy titles from '$pathToData/titles.csv' with (format csv, header true);" postgresql://postgres:postgres@localhost:5432/test
-}
-queryTablesWithFile() {
-  psql -f $1 postgresql://postgres:postgres@localhost:5432/test
+  psql -c "\copy departments from '$pathToData/departments.csv' with (format csv, header true);" $testDbLogin
+  psql -c "\copy employees from '$pathToData/employees.csv' with (format csv, header true);" $testDbLogin
+  psql -c "\copy dept_emp from '$pathToData/dept_emp.csv' with (format csv, header true);" $testDbLogin
+  psql -c "\copy dept_manager from '$pathToData/dept_manager.csv' with (format csv, header true);" $testDbLogin
+  psql -c "\copy salaries from '$pathToData/salaries.csv' with (format csv, header true);" $testDbLogin
+  psql -c "\copy titles from '$pathToData/titles.csv' with (format csv, header true);" $testDbLogin
 }
 queryTableNames() {
-  psql -c "\dt" postgresql://postgres:postgres@localhost:5432/test
+  psql -c "\dt" $testDbLogin
 }
 main 
