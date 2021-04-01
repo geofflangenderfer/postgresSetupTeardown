c4fe84adf4ca40b8865fd539742449577fb25608
Geoff Langenderfer <geoff.langenderfer@gmail.com>
2021-03-31 22:12:06 -0400

    improve formatting

--- run.sh
+++ run.sh
@@ -2,9 +2,8 @@
 
 # start postgres server
 sudo systemctl start postgresql@12-main
-./src/getQueryOutput.sh \
-  sql/schema.sql `# table creation logic` \
-  sql/queries.sql `# table query logic` \
-  data/ `# target data location` \
+./src/getQueryOutput.sh                               \
+  sql/schema.sql            ` # table creation logic` \
+  sql/queries.sql           ` # table query logic`    \
+  data/                     ` # target data location` \
   &> output/queryOutput.txt 
-
